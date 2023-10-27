c2a26e3547ed020e6f9570bc16154ab35632e078
UsmanJafri <usmanjafri@hotmail.com>
2023-10-27 15:49:18 -0400

    Updates

---
+++ _nuxt/Markdown.5f882b31.js
@@ -0,0 +1,1 @@
+import s from"./ContentSlot.b667d719.js";import{l as o,m,y as p,G as u}from"./entry.c8166a7e.js";import"./utils.01489156.js";import"./preview.d2691b0d.js";const d=o({name:"Markdown",extends:s,setup(t){const{parent:e}=u(),{between:n,default:r}=m(),a=p(()=>typeof t.unwrap=="string"?t.unwrap.split(" "):["*"]);return{fallbackSlot:r,tags:a,between:n,parent:e}}});export{d as default};

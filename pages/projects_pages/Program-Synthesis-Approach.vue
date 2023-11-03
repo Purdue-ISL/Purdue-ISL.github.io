7578ac8193e7543e8935f4c393a328d5bdaa1a5e
Abd AlRhman AlQiam <98279657+AlQiam@users.noreply.github.com>
2023-11-03 16:23:13 -0400

    Create Program-Synthesis-Approach.vue

---
+++ pages/projects_pages/Program-Synthesis-Approach.vue
@@ -0,0 +1,118 @@
+<template>
+  <div id="doc" class="bg-[var(--primary-color)] bg-img min-h-screen">
+    <div class="limit-width">
+      <HeaderNav />
+    </div>
+    <PageHeader title="Projects" />
+    <div class="limit-width">
+      <div class="w-[96%] mx-auto"></div>
+      <h1 class="subpages-title">
+        <!--Project title-->
+      </h1>
+      <SubPageNav
+        v-bind:data="[
+          ['Description', 'des'],
+          ['Publications', 'pub'],
+          ['People', 'ppl'],
+        ]"
+      />
+
+      <div id="des" class="subpages-section-container-div">
+        <div class="subpages-section-title-container-div">
+          <h1 class="subpages-section-title">Description</h1>
+        </div>
+        <div class="subpages-section-content">
+          <p>
+            <!--Project Description-->
+          </p>
+        </div>
+      </div>
+
+      <div id="pub" class="subpages-section-container-div">
+        <div class="subpages-section-title-container-div">
+          <h1 class="subpages-section-title">Publications</h1>
+        </div>
+        <div class="subpages-section-content">
+          <p>
+            <!--Project publications-->
+          </p>
+        </div>
+      </div>
+
+      <div id="ppl" class="subpages-section-container-div">
+        <div class="subpages-section-title-container-div">
+          <h1 class="subpages-section-title">Team</h1>
+        </div>
+        <div class="subpages-section-content">
+          <p>
+            <!--Project Team-->
+          </p>
+        </div>
+      </div>
+    </div>
+
+    <div class="pb-[30px]"></div>
+  </div>
+</template>  
+  
+  
+  
+  
+  
+  
+  <script type="module">
+let projects_divs = [];
+export default {
+  beforeRouteEnter(to, from, next) {
+    console.log(from);
+    console.log(to);
+    to.params.link = from;
+    next();
+  },
+  methods: {
+    overlap(element, threshold) {
+      let topOverlap =
+        (window.screen.height - element.getBoundingClientRect().top) /
+        window.screen.height;
+      if (topOverlap > threshold) {
+        return true;
+      }
+      return false;
+    },
+
+    handleScroll() {
+      var filtered = [];
+      for (var idx in projects_divs) {
+        if (this.overlap(projects_divs[idx], 0.1)) {
+          projects_divs[idx].classList.toggle("show-up");
+          filtered.push(idx);
+        }
+      }
+      for (var idx in filtered) {
+        delete projects_divs[filtered[idx]];
+      }
+      if (Object.keys(projects_divs).length == 0) {
+        console.log("removed");
+        window.removeEventListener("scroll", this.handleScroll);
+      }
+    },
+  },
+  mounted() {
+    window.scrollTo(0, 0);
+    projects_divs = [];
+    let projects_divs_nodelist = document.querySelectorAll(
+      ".subpages-section-container-div"
+    );
+    projects_divs_nodelist.forEach((projects_div) => {
+      projects_divs.push(projects_div);
+      if (projects_div.classList.contains("show-up")) {
+        projects_div.classList.toggle("show-up");
+      }
+    });
+
+    window.addEventListener("scroll", this.handleScroll);
+    // in case height is small.
+    this.handleScroll();
+  },
+};
+</script>

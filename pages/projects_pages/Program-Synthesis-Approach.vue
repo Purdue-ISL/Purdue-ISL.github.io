20fd2b7ea296e5f14771b24db2985aa18de24070
UsmanJafri <usmanjafri@hotmail.com>
2023-11-17 17:08:24 -0500

    Fix broken paper link on Program synthesis page

--- pages/projects_pages/Program-Synthesis-Approach.vue
+++ pages/projects_pages/Program-Synthesis-Approach.vue
@@ -86,7 +86,7 @@
               title="Learning Network Design Objectives Using A Program Synthesis Approach"
               authors="Yanjun Wang, Chuan Jiang, Xiaokang Qiu, Sanjay G. Rao"
               conference="Proceedings of the 18th ACM Workshop on Hot Topics in Networks (HotNets), 2019"
-              paper="papers/HotNets_2019_Paper.pdf"
+              paper="https://dl.acm.org/doi/10.1145/3365609.3365861"
             /><PublicationsSeparator />
           </p>
         </div>

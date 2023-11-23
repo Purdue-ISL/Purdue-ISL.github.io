c77adcd26a5294b9a5445e4a4ca32a7bf591842c
UsmanJafri <usmanjafri@hotmail.com>
2023-11-23 16:19:36 -0500

    Updates

--- _nuxt/ProseThead.f628553f.js
+++
@@ -1,1 +0,0 @@
-import{_ as o,o as r,c as t,Q as s}from"./entry.cf0ec55a.js";const a={};function c(e,n){return r(),t("thead",null,[s(e.$slots,"default")])}const _=o(a,[["render",c]]);export{_ as default};

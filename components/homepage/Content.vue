4d03ef3553bf707063d58769584184073b81ebc6
sgrlaf <sgrlaf@gmail.com>
2023-11-23 20:35:51 -0500

    Update Content.vue

--- components/homepage/Content.vue
+++ components/homepage/Content.vue
@@ -25,7 +25,7 @@
           </div>
           <div class="main-page-project-div">
             <h2 class="main-page-project-header">
-              ML-driven and next generation multi-perspective Internet video delivery
+              ML-driven and multi-perspective Internet video delivery
             </h2>
             <p class="main-page-project-content">
               We are optimizing Internet video delivery using ML techniques (such as causal 
